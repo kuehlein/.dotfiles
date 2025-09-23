e7c82e1bca6686cceb59a233247e24ede53862e9
kuehlein <kyle.uehlein@gmail.com>
2025-09-23 14:43:03 -0400

    Translate waybar config into nix

--- setups/sway/waybar/default.nix
+++ setups/sway/waybar/default.nix
@@ -9,128 +9,154 @@
     enable = true;
     settings = {
       mainbar = {
-        layer = "top";
+        height = 0; # 26;
+        layer = "bot"; # "top";
+	margin-bottom = 0;
+	margin-left = 500;
+	margin-right = 500;
+	margin-top = 15;
+        # output = [ "eDP-1" ];
         position = "top";
-        height = 26;
-        output = [ "eDP-1" ];
+	spacing = 0;
 
-        modules-left = [ "sway/workspaces" "sway/mode" ];
-        modules-right = [ "sway/language" "clock" "battery" ];
+        modules-left = [
+	  # "hyprland/mode" # ???
+	  "sway/workspaces"
+	];
+	modules-center = [
+	  "custom/applauncher"
+	];
+        modules-right = [
+	  "network"
+	  "battery"
+	  "pulseaudio"
+	  "tray"
+	  "clock"
+	  # "sway/language" # ???
+	];
+
+	# "hyprland/workspaces" = {
+	#   disable-scroll = true;
+	#   all-outputs = false;
+	#   tooltip = false;
+	# };
 
         "sway/workspaces" = {
           all-outputs = true;
           disable-scroll = true;
-          persistent_workspaces = {
-            "1" = [];
-            "2" = [];
-            "3" = [];
-            "4" = [];
+	  tooltip = false;
+          # persistent_workspaces = {
+          #   "1" = [];
+          #   "2" = [];
+          #   "3" = [];
+          #   "4" = [];
+          # };
+          # disable-click = true;
+        };
+
+        # TODO: i dont think i want this...
+        "custom/applauncher": {
+          format = "〇";
+          on-click = "pgrep wofi >/dev/null 2>&1 && killall wofi || wofi --show drun --location=top -y 15";
+          tooltip = false;
+        };
+        tray = {
+          spacing = 10;
+          tooltip = false;
+        };
+        clock = {
+          format = "󰅐 {:%H:%M}";
+          tooltip = false;
+        };
+        network = {
+          format-wifi = " {bandwidthDownBits}";
+          format-ethernet = " {bandwidthDownBits}";
+          format-disconnected = "󰤮 No Network";
+          interval = 5;
+          tooltip = false;
+        };
+        pulseaudio = {
+          scroll-step = 5;
+          max-volume = 150;
+          format = "{icon} {volume}%";
+          format-bluetooth = "{icon} {volume}%";
+          format-icons = [ "" "" " " ];
+          nospacing = 1;
+          format-muted = " ";
+          on-click = "pavucontrol";
+          tooltip = false;
+        };
+        battery = {
+          states = {
+            warning = 30;
+            critical = 15;
           };
-          disable-click = true;
+          format = "{icon} {capacity}%";
+          format-charging = "󰂄 {capacity}%";
+          format-plugged = "󰂄{capacity}%";
+          format-alt = "{icon} {time}";
+          format-full = "󱈑 {capacity}%";
+          format-icons = [ "󱊡" "󱊢" "󱊣" ];
         };
 
-        "sway/mode" = {
-          tooltip = false;
-        };
 
-        "clock" = {
-          interval = 60;
-          format = "{:%a %d %m %I:%M}";
-        };
-
-        "battery" = {
-          tooltip = false;
-        };
+        # "sway/mode" = {
+        #   tooltip = false;
+        # };
+        #
+        # "clock" = {
+        #   interval = 60;
+        #   format = "{:%a %d %m %I:%M}";
+        # };
+        #
+        # "battery" = {
+        #   tooltip = false;
+        # };
       };
     };
   };
 }
 
 
-    # etc."xdg/waybar/config".text = ''
-    #   {
-    #     "layer": "bot",
-    #     "spacing": 0,
-    #     "height": 0,
-    #     "margin-bottom": 0,
-    #     "margin-top": 15,
-    #     "position": "top",
-    #     "margin-right": 500,
-    #     "margin-left": 500,
-    #     "modules-left": [
-    #       "hyprland/workspaces",
-    #       "sway/workspaces"
-    #     ],
-    #     "modules-center": [
-    #       "custom/applauncher"
-    #     ],
-    #     "modules-right": [
-    #       "network",
-    #       "battery",
-    #       "pulseaudio",
-    #       "tray",
-    #       "clock"
-    #     ],
-    #     "hyprland/workspaces": {
-    #       "disable-scroll": true,
-    #       "all-outputs": false,
-    #       "tooltip": false
-    #     },
-    #     "sway/workspaces": {
-    #       "disable-scroll": true,
-    #       "all-outputs": false,
-    #       "tooltip": false
-    #     },
-    #     "custom/applauncher": {
-    #       "format": "〇",
-    #       "on-click": "pgrep wofi >/dev/null 2>&1 && killall wofi || wofi --show drun --location=top -y 15",
-    #       "tooltip": false
-    #     },
-    #     "tray": {
-    #       "spacing": 10,
-    #       "tooltip": false
-    #     },
-    #     "clock": {
-    #       "format": "󰅐 {:%H:%M}",
-    #       "tooltip": false
-    #     },
-    #     "network": {
-    #       "format-wifi": " {bandwidthDownBits}",
-    #       "format-ethernet": " {bandwidthDownBits}",
-    #       "format-disconnected": "󰤮 No Network",
-    #       "interval": 5,
-    #       "tooltip": false
-    #     },
-    #     "pulseaudio": {
-    #       "scroll-step": 5,
-    #       "max-volume": 150,
-    #       "format": "{icon} {volume}%",
-    #       "format-bluetooth": "{icon} {volume}%",
-    #       "format-icons": [
-    #         "",
-    #         "",
-    #         " "
-    #       ],
-    #       "nospacing": 1,
-    #       "format-muted": " ",
-    #       "on-click": "pavucontrol",
-    #       "tooltip": false
-    #     },
-    #     "battery": {
-    #       "states": {
-    #         "warning": 30,
-    #         "critical": 15
-    #       },
-    #       "format": "{icon} {capacity}%",
-    #       "format-charging": "󰂄 {capacity}%",
-    #       "format-plugged": "󰂄{capacity}%",
-    #       "format-alt": "{icon} {time}",
-    #       "format-full": "󱈑 {capacity}%",
-    #       "format-icons": [
-    #         "󱊡",
-    #         "󱊢",
-    #         "󱊣"
-    #       ]
-    #     }
-    #   }
-    # '';
+	#  programs.waybar = {
+	#    enable = true;
+	#    settings = {
+	#      mainbar = {
+	#        layer = "top";
+	#        position = "top";
+	#        height = 26;
+	#        output = [ "eDP-1" ];
+	#
+	#        modules-left = [ "sway/workspaces" "sway/mode" ];
+	#        modules-right = [
+	#   "sway/language"
+	#   "clock"
+	#   "battery"
+	# ];
+	#
+	#        "sway/workspaces" = {
+	#          all-outputs = true;
+	#          disable-scroll = true;
+	#          persistent_workspaces = {
+	#            "1" = [];
+	#            "2" = [];
+	#            "3" = [];
+	#            "4" = [];
+	#          };
+	#          disable-click = true;
+	#        };
+	#
+	#        "sway/mode" = {
+	#          tooltip = false;
+	#        };
+	#
+	#        "clock" = {
+	#          interval = 60;
+	#          format = "{:%a %d %m %I:%M}";
+	#        };
+	#
+	#        "battery" = {
+	#          tooltip = false;
+	#        };
+	#      };
+	#    };
