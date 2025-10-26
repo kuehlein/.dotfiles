a5e50b0fe938661c0aff9ba0d64767de955c6141
kuehlein <kyle.uehlein@gmail.com>
2025-10-25 21:08:47 -0400

    update flake lock

--- flake.nix
+++ flake.nix
@@ -17,28 +17,28 @@
     in {
       nixosConfigurations.t490 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-	specialArgs = { inherit neovim-config; };
+        specialArgs = { inherit neovim-config; };
         modules = [
           ./configuration.nix
           ./modules/common.nix
 
           ./setups/common.nix
-	  ./setups/sway/default.nix
-	  # ./setups/hyprland/default.nix
-	  # ./setups/dwm/default.nix
+          ./setups/sway/default.nix
+          # ./setups/hyprland/default.nix
+          # ./setups/dwm/default.nix
 
-	  home-manager.nixosModules.home-manager
-  	  {
-	    home-manager = {
-	      backupFileExtension = "backup";
-	      useGlobalPkgs = true; # Share system pkgs.
-	      useUserPackages = true; # Install user pkgs to profile.
-	      users = {
-	        kuehlein = import ./home/kuehlein.nix;
-	        kyle = import ./home/kyle.nix;
-	      };
-	    };
-	  }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              backupFileExtension = "backup";
+              useGlobalPkgs = true; # Share system pkgs.
+              useUserPackages = true; # Install user pkgs to profile.
+              users = {
+                kuehlein = import ./home/kuehlein.nix;
+                kyle = import ./home/kyle.nix;
+              };
+            };
+          }
         ];
       };
     };
